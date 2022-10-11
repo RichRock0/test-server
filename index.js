d10d96a32eeaa25b4456e338cdc59ff4bcb9f769
RichRock0 <113371063+RichRock0@users.noreply.github.com>
2022-10-10 23:15:13 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) =>{
+    res.send('Hello World Express');
+});
+
+app.listen(3000, () => {
+    console.log('App on port 3000')
+});
